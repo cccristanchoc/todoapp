684a0360b78bb435c53324f630b65dd8eb04ba6c
Cristian Cristancho <cccristanchoc@unal.edu.co>
2017-08-23 20:07:05 +0000

    html performance

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'welcome/contact'
+
+  get 'welcome/about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    
+    get '/welcome', to: 'welcome#index'
+    get '/contact', to: 'welcome#contact'
+    root 'welcome#about'
+    
 end
